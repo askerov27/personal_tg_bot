589c391533decf00605d15fd54662fe245723560
jaskerov <4xqnql5tlq425tjwoevob2qwws2araajnzpelsapol2xty76bvaq>
2024-01-21 20:12:41 +0700

    create bot

--- telegram.js
+++ telegram.js
@@ -1,29 +1,52 @@
 const TelegramBot = require("node-telegram-bot-api");
+const axios = require("axios");
 
 // Replace 'YOUR_BOT_TOKEN' with the token you received from BotFather.
-const token = "6478583836:AAFPcYLvyNLl5cdq_kO8WkgmxX8owhXj7TA";
+const TOKEN = "6478583836:AAFPcYLvyNLl5cdq_kO8WkgmxX8owhXj7TA";
+const CAT_API_URL = "https://api.thecatapi.com/v1/images/search";
+const DOG_API_URL = "https://api.thedogapi.com/v1/images/search";
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(TOKEN, { polling: true });
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
+bot.onText(/\хочу кысю/, async (msg) => {
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+  try {
+    const response = await axios.get(CAT_API_URL);
+    const data = response.data;
+
+    if (data && data.length > 0) {
+      const imageUrl = data[0].url;
+      bot.sendPhoto(chatId, imageUrl);
+    } else {
+      bot.sendMessage(chatId, "Unable to fetch cat picture. Try again later.");
+    }
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    bot.sendMessage(
+      chatId,
+      "An error occurred while fetching the cat picture."
+    );
+  }
 });
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on("message", (msg) => {
+bot.onText(/\давай собачку/, async (msg) => {
   const chatId = msg.chat.id;
 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, "update this");
+  try {
+    const response = await axios.get(DOG_API_URL);
+    const data = response.data;
+
+    if (data && data.length > 0) {
+      const imageUrl = data[0].url;
+      bot.sendPhoto(chatId, imageUrl);
+    } else {
+      bot.sendMessage(chatId, "Unable to fetch cat picture. Try again later.");
+    }
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    bot.sendMessage(
+      chatId,
+      "An error occurred while fetching the cat picture."
+    );
+  }
 });
